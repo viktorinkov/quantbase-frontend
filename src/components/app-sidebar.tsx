74ce1fa5708e73857a6880be2ee92e6d6c1c74e2
Viktor Minchev <vikitominchev@gmail.com>
2025-10-25 01:57:06 -0700

    Add Ducky logo SVG to AppSidebar component

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -141,7 +141,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
               <a href="#">
-                <IconInnerShadowTop className="!size-5" />
+                <img src="/ducky.svg" alt="Ducky Logo" className="!size-5" />
                 <span className="text-base font-semibold">QuantBase</span>
               </a>
             </SidebarMenuButton>
